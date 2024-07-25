92181e7b9a378c4a6c96e31e198f3952ec18931b
emmanuel <emmanuelajayi380@yahoo.co.uk>
2024-07-25 08:58:27 +0000

    add ssh setup but it is not fully functional yet

---
+++ ssh_setup_linux.sh
@@ -0,0 +1,5 @@
+# WORKFLOW IS NOT WORKING. FIX IT. PROBLEM LOOKS TO BE WITH THE SECOND COMMAND AFTER THE -f TAG
+# sudo apt install openssh-client && sudo apt install openssh-server
+# ssh-keygen -t rsa -f ~/.ssh/id_rsa <<< y
+# cat ~/.ssh/id_rsa.pub
+# ssh -T git@github.com
